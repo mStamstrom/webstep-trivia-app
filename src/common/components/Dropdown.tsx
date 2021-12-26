dc05f928490c2e254fe355bc0fc6a2c968e6d234
Marcus Stamström <marcus.stamstrom@gmail.com>
2021-12-26 12:22:20 +0100

    Adding dropdown component

---
+++ src/common/components/Dropdown.tsx
@@ -0,0 +1,16 @@
+interface Props extends React.SelectHTMLAttributes<HTMLSelectElement> {
+  label: string;
+  options: string[];
+}
+export const Dropdown = (props: Props) => {
+  return (
+    <label>
+      {props.label}
+      <select onChange={props.onChange}>
+        {props.options.map((option) => (
+          <option>{option}</option>
+        ))}
+      </select>
+    </label>
+  );
+};
