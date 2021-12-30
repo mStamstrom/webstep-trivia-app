77ec16b701a03a216d1a98f61291d2c6e9fba2be
Marcus Stamström <marcus.stamstrom@gmail.com>
2021-12-30 14:50:10 +0100

    As I user, I want to select an answer, see correct answer and only answer once

--- src/features/quiz-page/QuestionDisplay.tsx
+++ src/features/quiz-page/QuestionDisplay.tsx
@@ -1,17 +1,34 @@
-import { Question } from "../../common/requests/quizRequest";
+import clsx from "clsx";
+import { Answer, Question } from "../../common/requests/quizRequest";
 import styles from "./QuestionDisplay.module.css";
 
 interface Props {
   question: Question;
+  userAnswer: Answer | undefined;
+  onAnswer: (answer: Answer) => void;
 }
 
-export const QuestionDisplay = ({ question }: Props) => {
+export const QuestionDisplay = ({ question, userAnswer, onAnswer }: Props) => {
   return (
     <div>
       <h4 className={styles.question}>{question.question}</h4>
       <div className={styles.answerContainer}>
         {question.answers.map((answer, index) => (
-          <button className={styles.answer}>
+          <button
+            onClick={() => onAnswer(answer)}
+            disabled={!!userAnswer}
+            className={clsx([
+              styles.answer,
+              userAnswer && answer.isCorrectAnswer && styles.correctAnswer,
+              userAnswer &&
+                !userAnswer.isCorrectAnswer &&
+                userAnswer.answer === answer.answer &&
+                styles.wrongAnswer,
+              userAnswer &&
+                userAnswer.answer === answer.answer &&
+                styles.selectedAnswer,
+            ])}
+          >
             <div className={styles.index}>{index + 1}</div>
             <div className={styles.answerText}>{answer.answer}</div>
           </button>
--- src/features/quiz-page/QuizPage.tsx
+++ src/features/quiz-page/QuizPage.tsx
@@ -1,18 +1,44 @@
 import { useState } from "react";
-import { Question } from "../../common/requests/quizRequest";
+import { Answer, Question } from "../../common/requests/quizRequest";
 import { QuestionDisplay } from "./QuestionDisplay";
 interface Props {
   questions: Question[];
 }
 
+interface UserAnswer {
+  question: string;
+  answer: Answer;
+}
+
 export const QuizPage = (props: Props) => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [answers, setAnswers] = useState<UserAnswer[]>([]);
 
   const currentQuestion = props.questions[currentQuestionIndex];
+  const currentUserAnswer: UserAnswer | undefined =
+    answers[currentQuestionIndex];
+
+  const onAnswer = (selectedAnswer: Answer) => {
+    setAnswers([
+      ...answers,
+      {
+        question: currentQuestion.question,
+        answer: selectedAnswer,
+      },
+    ]);
+  };
 
   return (
     <div>
-      <QuestionDisplay question={currentQuestion} />
+      <div>
+        user has answered:{" "}
+        {currentUserAnswer ? currentUserAnswer.answer.answer : "No answer yet!"}
+      </div>
+      <QuestionDisplay
+        question={currentQuestion}
+        userAnswer={currentUserAnswer?.answer}
+        onAnswer={onAnswer}
+      />
     </div>
   );
 };
